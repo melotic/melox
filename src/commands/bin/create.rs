7af56988d65c3ae581c45e4310203c8bc43303e1
Justin Perez <26028834+melotic@users.noreply.github.com>
2023-09-04 20:01:40 -0700

    test(create): add tests

--- src/commands/bin/create.rs
+++ src/commands/bin/create.rs
@@ -38,3 +38,121 @@ pub async fn create_bin(
         edit_token,
     }))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::create_bin;
+    use crate::{
+        app_error::AppError,
+        database::MockDbClient,
+        encrypt::MockBinEncrypter,
+        models::{AppState, CreateBinRequest, DbBin},
+    };
+    use aes_gcm::aead::generic_array::GenericArray;
+    use axum::{extract::State, Json};
+    use mockall::predicate;
+    use std::sync::Arc;
+
+    fn create_mocks() -> (MockDbClient, MockBinEncrypter) {
+        let mock_db_client = MockDbClient::new();
+        let mock_encrypter = MockBinEncrypter::new();
+
+        (mock_db_client, mock_encrypter)
+    }
+
+    #[tokio::test]
+    async fn create_bin_happy_path() {
+        let bin_content = "hello world";
+        let (mut mock_db_client, mut mock_encrypter) = create_mocks();
+
+        mock_encrypter
+            .expect_generate_key_and_encrypt()
+            .with(predicate::eq(bin_content))
+            .returning(|_| {
+                Ok((
+                    *GenericArray::from_slice(&[0; 16]),
+                    *GenericArray::from_slice(&[0; 12]),
+                    vec![0; 16],
+                ))
+            });
+
+        mock_db_client
+            .expect_create_bin()
+            .with(predicate::function(|x: &DbBin| x.content == vec![0; 16]))
+            .returning(|_| Ok(()));
+
+        let app_state = AppState {
+            db_client: Box::new(mock_db_client),
+            encrypter: Box::new(mock_encrypter),
+        };
+
+        let req = CreateBinRequest {
+            content: bin_content.to_string(),
+        };
+
+        let res = create_bin(State(Arc::new(app_state)), Json(req)).await;
+
+        assert!(res.is_ok());
+    }
+
+    #[tokio::test]
+    async fn create_bin_encrypt_fail() {
+        let bin_content = "hello world";
+        let (mock_db_client, mut mock_encrypter) = create_mocks();
+
+        mock_encrypter
+            .expect_generate_key_and_encrypt()
+            .with(predicate::eq(bin_content))
+            .returning(|_| Err(anyhow::anyhow!("encrypt error").into()));
+
+        let app_state = AppState {
+            db_client: Box::new(mock_db_client),
+            encrypter: Box::new(mock_encrypter),
+        };
+
+        let req = CreateBinRequest {
+            content: bin_content.to_string(),
+        };
+
+        let res = create_bin(State(Arc::new(app_state)), Json(req)).await;
+
+        assert!(res.is_err());
+        assert!(matches!(res.unwrap_err(), AppError::InternalServerError(_)));
+    }
+
+    #[tokio::test]
+    async fn create_bin_db_fail() {
+        let bin_content = "hello world";
+        let (mut mock_db_client, mut mock_encrypter) = create_mocks();
+
+        mock_encrypter
+            .expect_generate_key_and_encrypt()
+            .with(predicate::eq(bin_content))
+            .returning(|_| {
+                Ok((
+                    *GenericArray::from_slice(&[0; 16]),
+                    *GenericArray::from_slice(&[0; 12]),
+                    vec![0; 16],
+                ))
+            });
+
+        mock_db_client
+            .expect_create_bin()
+            .with(predicate::function(|x: &DbBin| x.content == vec![0; 16]))
+            .returning(|_| Err(anyhow::anyhow!("db error").into()));
+
+        let app_state = AppState {
+            db_client: Box::new(mock_db_client),
+            encrypter: Box::new(mock_encrypter),
+        };
+
+        let req = CreateBinRequest {
+            content: bin_content.to_string(),
+        };
+
+        let res = create_bin(State(Arc::new(app_state)), Json(req)).await;
+
+        assert!(res.is_err());
+        assert!(matches!(res.unwrap_err(), AppError::InternalServerError(_)));
+    }
+}
